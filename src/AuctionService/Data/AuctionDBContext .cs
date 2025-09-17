483142cef63d687e0229580f0df9d627df218e18
unknown <bhaganagreshivendra3@gmail.com>
2025-09-17 23:19:01 +0530

    microservice carties

---
+++ src/AuctionService/Data/AuctionDBContext .cs
@@ -0,0 +1,26 @@
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDBContext : DbContext
+{
+    public AuctionDBContext(DbContextOptions<AuctionDBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Auction>()
+            .HasOne(a => a.Item)
+            .WithOne(i => i.Auction)
+            .HasForeignKey<Item>(i => i.AuctionId);
+
+        modelBuilder.Entity<Item>()
+            .Property(i => i.ImageUrl)
+            .IsRequired(false);
+    }
+}
